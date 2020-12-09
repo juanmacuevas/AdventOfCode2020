ccb809ebe6fdc47843b40f2f6fb072d5f8f9ea9a
JuanMa Cuevas <jumacuca@gmail.com>
2020-12-09 22:49:18 +0100

    tests7&8

---
+++ src/test/kotlin/Day07Test.kt
@@ -0,0 +1,34 @@
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+internal class Day07Test {
+
+    @Test
+    fun `calculate number of bags that could contain a shiny gold bag`() {
+        assertTrue(Day07().partOne() == 139)
+    }
+
+    @Test
+    fun `calculate number of bags required inside a shiny gold bag`() {
+        assertTrue(Day07().partTwo() == 58175)
+    }
+
+    @Test
+    fun `parse rule no other bags correctly`() {
+        val rule: Pair<String, List<String>> = Day07().ruleToMap("bright lavender bags contain no other bags.")
+        assertTrue(rule.first == "bright lavender")
+        assertTrue(rule.second.isEmpty())
+        assertEquals(("bright lavender" to emptyList<String>()), rule)
+    }
+
+    @Test
+    fun `parse rule contains other bags correctly`() {
+        val a: Pair<String, List<String>> =
+            Day07().ruleToMap("clear yellow bags contain 5 mirrored green bags, 4 striped red bags, 3 plaid magenta bags, 1 bright lavender bag.")
+        assertTrue(a.first == "clear yellow")
+        assertTrue(a.second.containsAll(listOf("mirrored green", "striped red", "plaid magenta", "bright lavender")))
+
+    }
+
+}
---
+++ src/test/kotlin/Day08Test.kt
@@ -0,0 +1,15 @@
+import org.junit.Test
+import kotlin.test.assertTrue
+
+internal class Day08Test {
+
+    @Test
+    fun partOne() {
+        assertTrue(Day08().partOne()==1709)
+    }
+
+    @Test
+    fun partTwo() {
+        assertTrue(Day08().partTwo()==1976)
+    }
+}
