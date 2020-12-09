97ce13ec0d1afa43940d65fa040ee86c8c13ff7c
JuanMa Cuevas <jumacuca@gmail.com>
2020-12-09 22:48:29 +0100

    day 10 prep

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -1,16 +1,13 @@
-import java.lang.Long.min
-
 class Day09 : AdventDay {
     override val dayNumber = this.javaClass.name.takeLast(2).toInt()
-//    private val groups = inputLines(dayNumber).splitOn("")
 
     override fun partOne(): Long {
         val nums = inputLines(dayNumber).map { it.toLong() }
         val window = 25
         (window..nums.size).forEach { i ->
             val set = mutableSetOf<Long>()
-            (i - window..i - 1).forEach { a ->
-                (i - window..i - 1).forEach { b ->
+            (i - window until i).forEach { a ->
+                (i - window until i).forEach { b ->
                     if (nums[a] != nums[b])
                         set.add(nums[a] + nums[b])
                 }
@@ -22,15 +19,14 @@ class Day09 : AdventDay {
         return -1
     }
 
-
     override fun partTwo(): Long {
         val total = partOne()
         val nums = inputLines(dayNumber).map { it.toLong() }
-        (0..nums.size - 1).forEach { i ->
+        (nums.indices).forEach { i ->
             var sum = nums[i]
             var mini = nums[i]
             var maxi = nums[i]
-            (i + 1..nums.size - 1).forEach { j ->
+            (i + 1 until nums.size).forEach { j ->
                 sum += nums[j]
                 mini = if (nums[j] < mini) nums[j] else mini
                 maxi = if (nums[j] > maxi) nums[j] else maxi
---
+++ src/main/kotlin/Day10.kt
@@ -0,0 +1,14 @@
+class Day10 : AdventDay {
+    override val dayNumber = this.javaClass.name.takeLast(2).toInt()
+
+    override fun partOne(): Long {
+        return -1
+    }
+
+    override fun partTwo(): Long {
+        return -1
+    }
+
+}
+
+
