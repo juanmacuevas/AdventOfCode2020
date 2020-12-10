db7b2aa146bbc30abeaa9359134a5b516f5d29e7
JuanMa Cuevas <jumacuca@gmail.com>
2020-12-10 17:44:34 +0100

    lean day 10

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -13,26 +13,22 @@ class Day10 : AdventDay {
         return (diffs[1]!! + 1) * (1 + diffs[3]!!)
     }
 
-    override fun partTwo(): Long {
-        val listAdjacent = mutableListOf(0)
-        inputLines(dayNumber)
-            .map { it.toInt() }
-            .sorted()
-            .toMutableList()
-            .apply {
-                add(0, 0)
+    override fun partTwo(): Long = inputLines(dayNumber).map { it.toInt() }
+        .sorted().toMutableList()
+        .apply { add(0, 0) }
+        .zipWithNext().map { it.second - it.first }
+        .fold(mutableListOf(0L), { acc, i ->
+            acc.apply {
+                if (i >= 3) {
+                    if (last() != 0L)
+                        add(0L)
+                } else add(removeLast() + 1L)
             }
-            .zipWithNext()
-            .map { it.second - it.first }
-            .fold(listAdjacent, { acc, i ->
-                acc.apply {
-                    add(if (i == 3) 0 else removeLast() + 1)
-                }
-            })
-        return listAdjacent
-            .map { n -> n * (n - 1) / 2 + 1L }
-            .reduce { acc, i -> (acc * i) }
-    }
+        })
+        .map { n -> n * (n - 1) / 2 + 1L }
+        .reduce { acc, i -> (acc * i) }
+
+
 }
 
 
